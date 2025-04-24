c8e9866aeee35edf2cf2af47b9d4af8754341583
Mark Olton <mark@olton.me>
2025-04-23 22:05:30 -0700

    * fix cors issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
